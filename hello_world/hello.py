5dbfba63bc6e733a7d9df1b65fad609b3b34faa5
Ijaz Ullah Khan <ijazullahkhan09@gmail.com>
2025-08-09 22:49:01 +0500

    practice variables and numpy

---
+++ hello_world/hello.py
@@ -0,0 +1,10 @@
+print("Welome to iuk family")
+
+
+def show(a):
+    print(a)
+
+show("that's awsm")
+
+
+name2= "iuk"
---
+++ hello_world/hello2.py
@@ -0,0 +1,7 @@
+import hello_world.hello as hello
+
+
+
+name = "LOVE love"
+
+hello.show(name)
